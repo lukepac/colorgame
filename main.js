29567fccaa233e43b5d170ab95e323260aa58590
LAP-LUKASZ\Łukasz <luk.paciorkowski@gmail.com>
2017-11-07 09:57:45 +0100

    easy hard buttons added

--- main.js
+++ main.js
@@ -4,7 +4,29 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
+easyBtn.addEventListener("click", function () {
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    colors = generateRandomColors(3);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+hardBtn.addEventListener("click", function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+});
 colorDisplay.textContent = pickedColor;
 for (var i = 0; i < squares.length; i++) {
     //add initial colors to squares
@@ -15,14 +37,18 @@ for (var i = 0; i < squares.length; i++) {
     //grab color of clicked square
     var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
+
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again!";
 
         }
+        
 
     });
 }
@@ -52,3 +78,15 @@ function randomColor() {
     var b = Math.floor(Math.random() * 256);
     return "rgb(" + r + ", " + g + ", " + b + ")";
 }
+
+resetButton.addEventListener("click", function(){
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+
+    }
+    h1.style.backgroundColor = "#232323";
+
+})
